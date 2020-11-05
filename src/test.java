e2ffa0b73523bf5a476d9d146ceb296fbc5d9707
Ostrich5yw_jys <1596253661@qq.com>
2020-11-05 18:12:11 +0800

    2020-11-05

--- src/test.java
+++ src/test.java
@@ -1,16 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class test {
-    private int[] mark = new int[100];
     public static void main(String[] args) {
         test t = new test();
-        System.out.println(t.searchMatrix(new int[][]{
-                                    {1, 3, 5, 7},
-                                    {10, 11, 16, 20},
-                                    {23, 30, 34, 60}
-                                  }, 3
-        ));
+        System.out.println(t.combine(4,2));
     }
-    public boolean searchMatrix(int[][] matrix, int target) {
-        return true;
+    public List<List<Integer>> combine(int n, int k){
+        List<List<Integer>> res = new ArrayList<>();
+        return res;
     }
 }
